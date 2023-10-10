be6f175aa0173184af378dcea8cb1c9378c440cd
GrownUprince <jake.lee4006@gmail.com>
2023-10-10 17:59:51 +0200

    chore: remove unnecessary config info of firebase

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -14,9 +14,6 @@ const firebaseConfig = {
   authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 // Initialize Firebase
 initializeApp(firebaseConfig);
